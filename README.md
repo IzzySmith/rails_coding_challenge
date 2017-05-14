711a522fe92403c7667bdb6a07b9b8983903b9c3
izzysmith <isobel@bridesmaid.org>
2017-05-14 16:58:48 +0100

    parsed JSON from the cities and displayed in a table

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ Weather comparison app
 A simple Rails application that compares the weather of 5 different cities. 
 
 
-###Approach 
+### Approach 
 
  - Make an intial API call (with multiple city ID's) to retrieve the information
  - Parse the JSON response and display this simply in the views 
+ - The JSON has key "item", and the value is  with a list all the cities data 
+ - The JSON is parsed and the data from the cities displayed in the view
--- WeatherApp/app/models/weather.rb
+++ WeatherApp/app/models/weather.rb
@@ -1,13 +1,14 @@
 class Weather
   include HTTParty
   
-    base_uri "http://api.openweathermap.org/data/2.5/group?appid=4122e656dc75ed86c90dde6ba95fbcd1"
+  base_uri "http://api.openweathermap.org/data/2.5/group?appid=4122e656dc75ed86c90dde6ba95fbcd1"
   format :json
 
   #call the api with HTTParty and parse the JSON response 
   def self.call list_ids
     response = HTTParty.get(base_uri + '&id=' + list_ids)
     body = JSON.parse(response.body)
+    list = body["list"]
   end
 
 end  
