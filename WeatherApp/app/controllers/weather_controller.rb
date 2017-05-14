0afc36989ce90d294103fc2525478e72bd0fa96f
izzysmith <isobel@bridesmaid.org>
2017-05-14 17:44:32 +0100

    displayed city names with best weather attributes

--- WeatherApp/app/controllers/weather_controller.rb
+++ WeatherApp/app/controllers/weather_controller.rb
@@ -2,5 +2,9 @@ class WeatherController < ApplicationController
   def index
     @cities = "524901,703448,6058560,1819729,4321929"
     @lookup = Weather.call(@cities)
+    @temp = Weather.max_value(Weather.make_hash(@lookup, "temp"))
+    @pressure = Weather.max_value(Weather.make_hash(@lookup, "pressure"))
+    @humidity = Weather.min_value(Weather.make_hash(@lookup, "humidity"))
+    @clouds = Weather.min_value(Weather.cloud_hash(@lookup))
   end
 end
--- WeatherApp/app/models/weather.rb
+++ WeatherApp/app/models/weather.rb
@@ -11,4 +11,36 @@ class Weather
     list = body["list"]
   end
 
+  #create a hash to store city : temp values 
+  def self.make_hash list, value
+    hash = Hash.new
+      list.each do |l|
+        temp = l["main"][value]
+        city = l["name"]
+        hash[city] = temp
+      end
+    return hash
+  end
+
+  #create hash with city: cloud cover
+  def self.cloud_hash list
+    hash = Hash.new
+      list.each do |l|
+        cloud = l["clouds"]["all"]
+        city = l["name"]
+        hash[city] = cloud
+      end
+    return hash
+  end
+
+  #returns the highest value from a hash
+  def self.max_value hash
+    hash.max_by{|k,v| v}
+  end  
+
+  #returns the lowest value from a hash
+  def self.min_value hash
+    hash.min_by{ |k,v| v}
+  end    
+
 end  
